a5be866574d44feb4bb09a70a3a200d40d213e36
oops <kisae3001@gmail.com>
2020-04-13 22:15:33 +0900

    push test

---
+++ src/test/java/book1/web/HelloControllerTest.java
@@ -0,0 +1,34 @@
+package book1.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello() throws Exception{
+        String hello = "hello1";
+        mvc.perform(get("/hello"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(hello))
+        ;
+
+
+
+    }
+}
